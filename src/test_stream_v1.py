f4710cbceb3446dc1c5e0b11cfed43672eb08fa3
montaquila <akiel@akiel.org>
2022-08-24 13:20:08 -0700

    making more updates to comments in v1.py

--- src/test_stream_v1.py
+++ src/test_stream_v1.py
@@ -1,4 +1,4 @@
-"""
+""":
 Messy but working Python program using RTSP protocol to stream video
 from the l51dm Annke turret IP Cam using openCV and displaying it on 
 a simple webpage using Flask.
@@ -102,33 +102,42 @@ def generate():
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
             bytearray(encoded_img) + b'\r\n')
 
+"""
+Route this method to *url*/video_feed
+"""
 @app.route("/video_feed")
 def video_feed():
     # return the response generated along with the specific media
     # type (mime type)
     return Response(generate(),
         mimetype = "multipart/x-mixed-replace; boundary=frame")
-
-# check to see if this is the main thread of execution
+"""
+check to see if this is the main thread of execution
+pass 
+"""
 if __name__ == '__main__':
     # construct the argument parser and parse command line arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ip", type=str, required=False, default='192.168.1.63',
-        help="ip address of the device")
+    """
+    #ap.add_argument("-i", "--ip", type=str, required=False, default='192.168.1.176',
+        #help="ip address of the device")
     ap.add_argument("-o", "--port", type=int, required=False, default=8000, 
         help="ephemeral port number of the server (1024 to 65535)")
+    """
     ap.add_argument("-f", "--frame-count", type=int, default=32,
         help="# of frames used to construct the background model")
+   
     args = vars(ap.parse_args())
-
-    t = threading.Thread(target=stream, args=(args["frame_count"],))
-    t.daemon = True
-    t.start()
- 
+    
+    thr_arg = threading.Thread(target=stream, args=(args["frame_count"],))
+    thr_arg.daemon = True
+    thr_arg.start()
+    
     # start the flask app
     #app.run(host=args["ip"], port=args["port"], debug=True,
     #    threaded=True, use_reloader=False)
     app.run(host= '0.0.0.0',debug=True)
+
 # release the video stream pointer
 capture.release()
 cv2.destroyAllWindows()
