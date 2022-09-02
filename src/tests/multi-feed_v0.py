d226e6c77cb4d91e8a32e92b3b55c6683c6c2584
montaquila <akiel@akiel.org>
2022-09-01 22:50:22 -0700

    attempting different approach to getting multi-feed with IP-cameras

--- src/tests/multi-feed_v0.py
+++ src/tests/multi-feed_v0.py
@@ -23,20 +23,6 @@ import threading
 
 #os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
-# ip cam #1 local environment IP
-
-# ip cam #1 test-site
-src_url1 = cv2.VideoCapture('rtsp://admin:password123@disc-cam2.iot.nau.edu:554/2')
-# ip cam #2 local env IP
-
-# ip cam #2 test-site
-#src_url2 = cv2.VideoCapture('rtsp://admin:password123@10.55.34.251:554/2')
-# ip cam #3 test-site
-src_url3 = cv2.VideoCapture('rtsp://admin:password123@disc-cam3.iot.nau.edu:554/2')
-# ip cam #1 test-site IP
-#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.251:554/2')
-#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.46:554/2')
-
 # define window variables and size allowing for resizing when program runs
 window1 = "window1"
 #window2 = "window2"
@@ -50,56 +36,55 @@ cv2.resizeWindow(window1, 720, 720)
 #cv2.resizeWindow(window2, 720, 720)
 cv2.resizeWindow(window3, 720, 720)
 
-
+# ip cam #1 test-site
 src_url1 = cv2.VideoCapture('rtsp://admin:password123@disc-cam2.iot.nau.edu:554/2')
+# ip cam #2 local env IP
 
+# ip cam #2 test-site
+#src_url2 = cv2.VideoCapture('rtsp://admin:password123@10.55.34.251:554/2')
+# ip cam #3 test-site
 src_url3 = cv2.VideoCapture('rtsp://admin:password123@disc-cam3.iot.nau.edu:554/2')
+# ip cam #1 test-site IP
+#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.251:554/2')
+#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.46:554/2')
 
 # method for capturing from our IP cameras
 def capture():
     if src_url1.isOpened() and src_url3.isOpened():
 
-        #var1 = src_url1.get(cv2.CAP_PROP_BUFFERSIZE)
+        var1 = src_url1.get(cv2.CAP_PROP_BUFFERSIZE)
     #var2 = src_url2.get(cv2.CAP_PROP_BUFFERSIZE)
-        #var3 = src_url3.get(cv2.CAP_PROP_BUFFERSIZE)
+        var3 = src_url3.get(cv2.CAP_PROP_BUFFERSIZE)
 
-        #src_url1.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+        src_url1.set(cv2.CAP_PROP_BUFFERSIZE, 3)
     #src_url2.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-        #src_url3.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+        src_url3.set(cv2.CAP_PROP_BUFFERSIZE, 3)
 
-        #start_frame_num = 20
-        #src_url1.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
+        start_frame_num = 20
+        src_url1.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
     #src_url2.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
-        #src_url3.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
+        src_url3.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
     
-    #if src_url1.isOpened() and src_url3.isOpened():
-
-    #while (ret1):
         # ret; bool val returns true if fram is available
         # frame; img arr vector 
         ret1, frame1 = src_url1.read()
         #ret2, frame2 = src_url2.read()
         ret3, frame3 = src_url3.read()
         # resize our frames
-        #cam_frame1 = cv2.resize(frame1, (0, 0), fx=.50,fy=.50)
+        cam_frame1 = cv2.resize(frame1, (0, 0), fx=.50,fy=.50)
         #cam_frame2 = cv2.resize(frame2, (0, 0), fx=.50,fy=.50)
-        #cam_frame3 = cv2.resize(frame3, (0, 0), fx=.50,fy=.50)
+        cam_frame3 = cv2.resize(frame3, (0, 0), fx=.50,fy=.50)
     else:
         ret1 = False
         ret3 = False
 
     while ret1 and ret3:
-        
-        var1 = src_url1.get(cv2.CAP_PROP_BUFFERSIZE)
-        var3 = src_url3.get(cv2.CAP_PROP_BUFFERSIZE)
+        # ret; bool val returns true if fram is available
+            # frame; img arr vector
+        ret1, frame1 = src_url1.read()
+        #ret2, frame2 = src_url2.read()
+        ret3, frame3 = src_url3.read()
 
-        src_url1.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-        src_url3.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-
-        start_frame_num = 20
-
-        src_url1.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
-        src_url3.set(cv2.CAP_PROP_POS_FRAMES, start_frame_num)
         # depending on how cam is mounted, adjust this line
             # for this instance we flip the camera vertically
         cam_frame1 = cv2.flip(frame1, 0)
---
+++ src/tests/multi-feed_v2.py
@@ -0,0 +1,58 @@
+"""
+Messy but working Python program using RTSP protocol to stream video
+from the l51dm Annke turret IP Cam using openCV. 
+
+- first declare the window this video output will be displayed on
+and enable it to be resized. 
+- declare capture object and pass in the full rtsp cmd
+- confirm capture is opened 
+- original orientation of camera if placed on flat side down, displays
+upside down, used cv2.flip to flip cam vertically
+- 
+
+current issues: 
+- when flipping orientation of camera output, must also flip the timestamp 
+
+"""
+
+import cv2
+
+window = "top"
+cv2.namedWindow(window, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window, 720, 720)
+
+# ip cam #1 local environment IP
+#capture_obj = cv2.VideoCapture('rtsp://admin:password123@192.168.1.177:554/2')
+capture_obj = cv2.VideoCapture('rtsp://admin:password123@disc-cam3.iot.nau.edu:554/2')
+# ip cam #1 test-site IP
+#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.251:554/2')
+#capture_obj = cv2.VideoCapture('rtsp://admin:password123@10.55.34.46:554/2')
+
+
+def capture():
+    if capture_obj.isOpened():
+        # ret; bool val returns true if fram is available
+        # frame; img arr vector 
+        ret, frame = capture_obj.read()
+    else:
+        ret = False
+    while ret:
+        ret, frame = capture_obj.read()
+        # depending on how cam is mounted, adjust this line
+            # for this instance we flip the camera vertically
+        stream = cv2.flip(frame, 0);
+        
+        # cv2.imshow("Capturing", frame)
+        cv2.imshow(window, stream)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    capture_obj.release()
+    cv2.destroyAllWindows()
+
+def main():
+    capture()
+
+if __name__ == "__main__":
+    main()
+
