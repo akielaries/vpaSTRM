f3a546232c271d60d3aae34c05eb3681d886ac69
montaquila <akiel@akiel.org>
2022-09-13 14:36:16 -0700

    moving previous versions of public test to tests folder. Updating comments and vars in pub test

--- src/main/pub-test_v1.py
+++ src/main/pub-test_v1.py
@@ -1,3 +1,12 @@
+"""
+similar to previous versions of our stream implementations 
+this time we are not using threading, creating a function to 
+parse our cameras to assign URL extensions based off of, and 
+render our stream using the index.html template scene in /tempaltes
+
+The biggest issue with this is latency and buffering issues.
+"""
+
 from flask import Flask, render_template, Response
 import cv2
 
@@ -10,22 +19,23 @@ def parse_cams(id):
 
     return cameras[int(id)]
 
-def gen_frames(camera_id):
+def gen_frames(cam_id):
 
-    cam = parse_cams(camera_id)
-    cap=  cv2.VideoCapture(cam)
+    cam = parse_cams(cam_id)
+    capture =  cv2.VideoCapture(cam)
 
     while True:
         # for cap in caps:
         # # Capture frame-by-frame
-        success, frame = cap.read()  # read the camera frame
+        success, frame = capture.read()  # read the camera frame
         if not success:
             break
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                # concat frame one by one and show result
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed/<string:id>/', methods=["GET"])
