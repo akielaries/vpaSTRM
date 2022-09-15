bbb28597492fdb9892fd86a9cf3ef353555d5b37
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2022-09-15 11:41:05 -0700

    adding comments

--- src/main/public_v5.py
+++ src/main/public_v5.py
@@ -6,8 +6,6 @@ render our stream using the index.html template scene in /tempaltes
 
 The biggest issue with this is latency and buffering issues.
 """
-
-from flask import Flask, render_template, Response
 # import the necessary packages
 from flask import Response, Flask, render_template
 import threading
@@ -17,18 +15,49 @@ import datetime, time
 import imutils
 import cv2
 
-
+"""
+initialize the output frame and a lock used to ensure thread-safe
+exchanges of the output frames (useful when multiple browsers/tabs are viewing the stream)
+"""
+output_frame = None
+thr_lock = threading.Lock()
+ 
+# initialize a flask object
 app = Flask(__name__)
 
-output_frame = None
-t_lock = threading.Lock()
-
+"""
+method for parsing our cam ids. declared as a list
+declare capture object and pass in the full rtsp cmd and 
+confirm capture is opened 
+RTSP format for l51dm cameras :
+    - rtsp://user:pass@IPADDRESS:554/channel
+        * 554 = default RTSP port
+"""
 def parse_cams(id):
     cameras = ['rtsp://admin:password123@disc-cam2.iot.nau.edu:554/2',
                'rtsp://admin:password123@disc-cam3.iot.nau.edu:554/2']
 
     return cameras[int(id)]
 
+"""
+stream_feed():
+    - set output and thread lock variables and read in the frames
+    - set a stream var = to our openCV's flip method for correct orientation
+    - resize our streams window in the webpage 
+<----- LEAVE OFF HERE  ----->
+    - rethink this method especially. other non-threaded version works fine 
+    without this. perhaps morph stream_feed into a resize function since we 
+    do not need to display anything using the OpenCV windows, only the HTML
+    webpage we are creating for localhost. 
+    - maybe pass a camera object into here and return the new resized frame
+    OVERALL THINK OF A NEW WAY TO USE THIS 
+<----- PICK UP HERE  ----->
+    - will want to remove the code that displays our feeds in an 
+    openCV window
+    - rethink generate() and stream_feed() to communicate off eachother
+    
+    generate() - should do all of our streaming work. 
+"""
 def stream_feed(cam_id, frame_count):
     global output_frame, t_lock
 
@@ -43,7 +72,16 @@ def stream_feed(cam_id, frame_count):
                 output_frame = frame.copy()
         
     return cam_id, output_frame
+
 """
+generate():
+    - enter a loop where we will encode the cameras stream as 
+    jpg files
+    - yield our output frames in byte formate 
+issues: 
+    - video(or rather continuous photo) stream is rather choppy
+    not sure if any improvements could be made in here
+
 <----LEAVE OFF HERE---->
 """
 def generate(cam_id):
@@ -72,17 +110,40 @@ def generate(cam_id):
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + convert_to_bytes + b'\r\n')
 
+"""
+video_feed():
+    - returns streaming response
+    - returns the images to be displayed to the web page
+    - browsers support multipart responses allowing for a continuous
+    stream of pictures appearing as video
+    - route this method to *url*/cam_1 using @app.route
+    - would want to modify/duplicate for display of multiple cameras
+    or potentially create another script for each camera using this as a
+    template
+issues : 
+    - when visiting the url, loads infinitely
+"""
 @app.route('/video_feed/<string:id>/', methods=["GET"])
 def video_feed(id):
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(generate(id),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
+"""
+index():
+    - return the template store in templates/index.html
+** "routes" are essentially URLs of our application (i.e. 192.168.1.176)
+"""
 @app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
+"""
+- check to see if this is the main thread of execution
+- pass in our arguments to get our applicatoin running
+    * creation of argument parser var, pass in frame count and host,
+    return
+"""
 def main():
     cam_id = 0
     frame_count = output_frame
@@ -111,5 +172,5 @@ def main():
 
 
 if __name__ == '__main__':
+    # run main
     main()
-
