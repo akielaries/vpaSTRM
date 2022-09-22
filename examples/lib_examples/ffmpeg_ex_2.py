cef73689f486cbff69ef030d736aca1adc0a06ee
montaquila <akiel@akiel.org>
2022-09-22 01:39:15 -0700

    removing ffmpeg examples

--- examples/lib_examples/ffmpeg_ex_2.py
+++
@@ -1,9 +0,0 @@
-import ffmpeg
-
-
-stream = ffmpeg.input("rtsp://admin:password123@disc-cam2.iot.nau.edu:554/2", ss=0)
-file = stream.output("test_stream.mp4")
-test = file.run(capture_stdout=True, capture_stderr=True)
-
-
-
