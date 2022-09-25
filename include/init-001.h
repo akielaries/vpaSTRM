16e95ee8d64cd1036f88b99476909debf4276ce3
montaquila <akiel@akiel.org>
2022-09-24 23:52:44 -0700

    adding example to capture STDOUT->log file. Perhaps create new log every iteration for next time i sit down for this.

--- include/init-001.h
+++ include/init-001.h
@@ -4,6 +4,9 @@
 
 #ifndef INIT001_h
 #define INIT001_H
+
+void print_str(char **str);
+int return_err(char *str);
 void log_dump();
 
 #endif
--- src/vid/init-001.c
+++ src/vid/init-001.c
@@ -4,24 +4,60 @@
  * and call some functions
  */
 
-#include "../../include/vid/init-001.h"
+#include "../../include/init-001.h"
 #include <stdio.h>
 
+enum{SUCCESS, FAIL, STR_NUM = 6};
+
+
+void print_str(char **str) {
+    int index;
+
+    for (index = 0; index < STR_NUM; index++) 
+        printf("%s \n", str[index]);
+}
+
+int return_err(char *str) {
+    printf("ERROR:  CANNOT OPEN %s \n", str);
+
+    return FAIL;
+}
 
 /*
  * Function that will 'dump' our log data to a .txt file
+ * essentially redirecting STDOUT->.txt
  */
-void log_dump() {
-    FILE *file_ptr;
+//void log_dump() {
+    //FILE *file_ptr;
+    //file_ptr = fopen("../../logs/", "w+");
     
-    file_ptr = fopen()
+    
+//}
 
-}
+int main(void) {
+    //log_dump();
 
-int main() {
-    log_dump();
+    char *str[STR_NUM] = {
+        "Redirecting a standard stream to the text file.",
+        "These 5 lines of text will be redirected"
+        "so many things you can do if you understand the",
+        "concept, fundamental idea - try this one!",
+        "--------------DONE--------------------------"
+    };
+    char filename[] = "../../logs/test-log-001.txt";
+    int reval = SUCCESS;
 
-    return 0;
+    print_str(str);
+    
+    if (freopen(filename, "w", stdout) == NULL) {
+        reval = return_err(filename);
+    }
+    else {
+        print_str(str);
+        fclose(stdout);
+    }
+
+    return reval;
 }
 
 
