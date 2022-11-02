8d62b90f3c9cbdfd89fb033a32bf72969a00813c
montaquila <akiel@akiel.org>
2022-11-02 00:12:45 -0700

    testing build workflow

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# script to compile and run our program. using just makefiles is a pain :(
+
+# rebuild our program 
+make compile 
+
+# run but with arguments passed in and 
+# write errors and normal output to log file
+
+
+# run bin, display to console AND write to .txt file
+#./bin/run/convert-001 "$@" | tee logs/STDOUT-001.txt
+./bin/run/vpaSTRM-1x "$@" 
+
+# run bin, display stderr and stdout to seperate files
+
